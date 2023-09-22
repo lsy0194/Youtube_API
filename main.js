4cc3778840beb3f0f65e2dba946a5c87f5d20186
lsy0194 <tktmadltmddu@naver.com>
2023-09-22 11:39:08 +0900

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+const api_key = 'AIzaSyC-pvI3J0WNJY2yI82H6PGggEgUR-Hv5iY';
+const baseurl = 'https://www.googleapis.com/youtube/v3/playlistItems';
+const pid = 'PLNiucQiR7LtQSLOd5hZS7199O5xS7PWPO';
+const num = 5;
+const resulturl = `${baseurl}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+fetch(resulturl)
+	.then((data) => data.json())
+	.then((json) => {
+		console.log(json);
+	});
