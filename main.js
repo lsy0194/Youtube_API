7f01786d764f0bf8ee79f698478edfa5315439f9
lsy0194 <tktmadltmddu@naver.com>
2023-09-22 16:40:07 +0900

    최종 코드 정리

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//전역변수 정보값
+//외부데이터 fetching시 준비사항, api-key발급,쿼리값
+//api-key, 가져올 데이터갯수, 플레이리스트아이디
+//위의 쿼리값들을 조합해서 최종 데이터 요청 url완성
 const frame = document.querySelector('section');
 const api_key = 'AIzaSyC-pvI3J0WNJY2yI82H6PGggEgUR-Hv5iY';
 const baseurl = 'https://www.googleapis.com/youtube/v3/playlistItems';
@@ -7,29 +11,31 @@ const resulturl = `${baseurl}?key=${api_key}&part=snippet&playlistId=${pid}&maxR
 const tit_len = 50;
 const desc_len = 180;
 
-//이벤트 위임(event delegation)
-//동적으로 생성되는 요소에 이벤트 연결이 불가, 이벤트 연결시점에는 생성이 되지않았기 떄문
-//항상있는 body요소에다가 이벤트를 위임을해서 추후 동적 dom이 생기면 이벤트를 전달받도록 처리
+//동적으로 생성되는 요소애는 무조건 window에 이벤트 위임을 걸고
 window.addEventListener('click', (e) => {
-	//e.currentTarget : 이벤트가 연결이 되어있는 선택자를 반환
-	//e.target : 실제화면상에서 이벤트가 발생한 요소를 반환
-
+	// 선택자 요소에 따라 다른 다른 함수를 호출
+	//썸네일 클릭시 팝업 호출
 	if (e.target.nodeName === 'IMG') createPop(e.target.getAttribute('data-vid'));
+	//닫기버튼 클릭시 팝업 제거
 	if (e.target.className === 'close') removePop();
 });
+//브라우저 로딩시 fetch함수 호출
 
 fetch(resulturl)
+	//promise로 넘어온 데이터를 동기적으로 받아서 json형태로 parsing한 다음에
 	.then((data) => data.json())
 	.then((json) => {
 		console.log(json.items);
 
 		let tags = '';
-
+		//
+		//동적으로 목록생성
 		json.items.map((data) => {
+			//글자값이 길어서 글자짜르기로 가공처리
 			let desc = data.snippet.description;
 			desc.length > desc_len ? (desc = desc.substr(0, desc_len) + '...') : desc;
-
-			//날짜값 가공
+			//parsing된 배열데이터를 반복돌면서
+			//동적으로 목록 생성
 			let date = data.snippet.publishedAt.split('T')[0];
 			date = date.split('-').join('.');
 
@@ -55,6 +61,7 @@ fetch(resulturl)
 		frame.innerHTML = tags;
 	});
 
+//이벤트위임으로 썸네일 클릭시 동적으로 팝업호출시 영상출력 함수
 function createPop(id) {
 	const aside = document.createElement('aside');
 	aside.innerHTML = `
@@ -66,7 +73,7 @@ function createPop(id) {
 	document.body.append(aside);
 	document.body.style.overflow = 'hidden';
 }
-
+//이벤트위임으로 닫기 버튼클릭시 팝업 제거하는 함수
 function removePop() {
 	const pop = document.querySelector('aside');
 	pop.remove();
