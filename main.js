fe858544002374a24ce5a00968ea9aaa5ca94c70
lsy0194 <tktmadltmddu@naver.com>
2023-09-22 14:15:38 +0900

    본문내용 자르기

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const pid = 'PLNiucQiR7LtQSLOd5hZS7199O5xS7PWPO';
 const num = 5;
 const resulturl = `${baseurl}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 const tit_len = 50;
+const desc_len = 180;
 
 //일정 글자수 이상일때 글자짜르고 말줄임표 붙이기
 //문자열.substr(시작위치,자를 글자수)
@@ -17,15 +18,18 @@ fetch(resulturl)
 		let tags = '';
 
 		json.items.map((data) => {
+			let desc = data.snippet.description;
+			desc.length > desc_len ? (desc = desc.substr(0, desc_len) + '...') : desc;
+
 			tags += `
         <article>
           <h2>${
 						data.snippet.title.length > tit_len
-							? data.snippet.title.substr(0, 30) + '...'
+							? data.snippet.title.substr(0, tit_len) + '...'
 							: data.snippet.title
 					}</h2>
           <div class="txt">
-            <p>${data.snippet.description}</p>
+            <p>${desc}</p>
             <span>${data.snippet.publishedAt}</span>
           </div>
           <div class="pic">
