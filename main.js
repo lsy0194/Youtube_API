66154d2952ce997d37adc5e9d7da06dcbdf36599
lsy0194 <tktmadltmddu@naver.com>
2023-09-22 15:38:52 +0900

    동적인 요소에 이벤트 위임 처리

--- main.js
+++ main.js
@@ -7,19 +7,17 @@ const resulturl = `${baseurl}?key=${api_key}&part=snippet&playlistId=${pid}&maxR
 const tit_len = 50;
 const desc_len = 180;
 
-//일정 글자수 이상일때 글자짜르고 말줄임표 붙이기
-//문자열.substr(시작위치,자를 글자수)
+//이벤트 위임(event delegation)
+//동적으로 생성되는 요소에 이벤트 연결이 불가, 이벤트 연결시점에는 생성이 되지않았기 떄문
+//항상있는 body요소에다가 이벤트를 위임을해서 추후 동적 dom이 생기면 이벤트를 전달받도록 처리
+window.addEventListener('click', (e) => {
+	//e.currentTarget : 이벤트가 연결이 되어있는 선택자를 반환
+	//e.target : 실제화면상에서 이벤트가 발생한 요소를 반환
 
-//특정 구분자를 기점으로 문자를 잘라서 배열로 반환하기
-//문자에서 특정 기호나 문자값을 일괄적으로 변경하고 싶을때
-
-//beef-lettuce-tomato --> Beef Lettuce Tomato
-let text = 'beef-lettuce-tomat';
-text = text
-	.split('-') //기존문자열에서 -을 기준으로 배열로 분리
-	.map((el) => el.charAt(0).toUpperCase() + el.slice(1)) //분리된 문자값을 반복돌면서 첫번째 글자만 대문자변경+첫번째를 제외만 나머지 문자 이어붙임(각단어의 첫 글자만 대문자로 변경되서 배열로 반환)
-	.join(' '); //첫글지만 대문자로 변경된 단어들을 다시 빈킨으로 이어붙이기
-console.log(text);
+	if (e.target.nodeName === 'IMG') {
+		console.log('You clicked Pic');
+	}
+});
 
 fetch(resulturl)
 	.then((data) => data.json())
