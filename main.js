b659d6a99a0b039a9f8423ea1f41118dc14aa874
lsy0194 <tktmadltmddu@naver.com>
2023-09-22 11:40:37 +0900

    유튜브 데이터 호출

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const api_key = 'AIzaSyC-pvI3J0WNJY2yI82H6PGggEgUR-Hv5iY';
 const baseurl = 'https://www.googleapis.com/youtube/v3/playlistItems';
 const pid = 'PLNiucQiR7LtQSLOd5hZS7199O5xS7PWPO';
-const num = 5;
+const num = 3;
 const resulturl = `${baseurl}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
 fetch(resulturl)
