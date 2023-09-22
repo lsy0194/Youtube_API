91768e90d6d02a1ef887a3f7e38b95b2ce7251df
lsy0194 <tktmadltmddu@naver.com>
2023-09-22 14:08:27 +0900

    제목 글자 자르기

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const baseurl = 'https://www.googleapis.com/youtube/v3/playlistItems';
 const pid = 'PLNiucQiR7LtQSLOd5hZS7199O5xS7PWPO';
 const num = 5;
 const resulturl = `${baseurl}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+const tit_len = 50;
+
+//일정 글자수 이상일때 글자짜르고 말줄임표 붙이기
+//문자열.substr(시작위치,자를 글자수)
 
 fetch(resulturl)
 	.then((data) => data.json())
@@ -15,7 +19,11 @@ fetch(resulturl)
 		json.items.map((data) => {
 			tags += `
         <article>
-          <h2>${data.snippet.title}</h2>
+          <h2>${
+						data.snippet.title.length > tit_len
+							? data.snippet.title.substr(0, 30) + '...'
+							: data.snippet.title
+					}</h2>
           <div class="txt">
             <p>${data.snippet.description}</p>
             <span>${data.snippet.publishedAt}</span>
